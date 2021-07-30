de1672b7acd2410d9957294a743256ae9f596481
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-07-30 19:18:05 +0530

    fn progs done

---
+++ apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -0,0 +1,6 @@
+// Only change code below this line
+function urlSlug(title) {
+return title.trim().toLowerCase().split(/\s+/).join('-');
+
+}
+// Only change code above this line
---
+++ return-a-sorted-array-without-changing-the-original-array.js
@@ -0,0 +1,11 @@
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  
+  return [].concat(arr).sort(function(a,b) {
+    return a-b;
+  });
+
+  // Only change code above this line
+}
+nonMutatingSort(globalArray);
